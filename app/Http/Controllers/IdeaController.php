97eab3a875bcb7557477e15a9816ef5e1cbd0a22
RickyGrajales <luisgrajales@gmail.com>
2024-10-26 11:40:40 -0500

    Se modificó y actualizo los componentes de IdeaController e index.blade.php

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Idea;
+use Illuminate\Support\Facades\DB;
 
 class IdeaController extends Controller
 {
     //
     public function index()
     {
-        return view('ideas.index');
+      $ideas = DB::table('ideas')->get(); // esto seria un select * from ideas 
+        return view('ideas.index', ['ideas' => $ideas]);
     }
 }
